e90e76098cd29dc774a9afcb6f2bd7b9c32cef10
Discussions <80537729+discus-sions@users.noreply.github.com>
2026-04-11 15:40:22 -0500

    Fix Viper not being counted as impostor

--- src/Extensions/RoleTypesExtension.cs
+++ src/Extensions/RoleTypesExtension.cs
@@ -4,7 +4,12 @@ namespace Lotus.Extensions;
 
 public static class RoleTypesExtension
 {
-    public static bool IsImpostor(this RoleTypes roleTypes) => roleTypes is RoleTypes.Impostor or RoleTypes.Shapeshifter or RoleTypes.ImpostorGhost or RoleTypes.Phantom;
+    public static bool IsImpostor(this RoleTypes roleTypes) => roleTypes is RoleTypes.Impostor
+        or RoleTypes.Shapeshifter
+        or RoleTypes.ImpostorGhost
+        or RoleTypes.Phantom
+        or RoleTypes.Viper;
+    
     public static bool IsCrewmate(this RoleTypes roleTypes) => !IsImpostor(roleTypes);
 
     public static RoleTypes GhostEquivalent(this RoleTypes roleTypes) => roleTypes.IsImpostor() ? RoleTypes.ImpostorGhost : RoleTypes.CrewmateGhost;
